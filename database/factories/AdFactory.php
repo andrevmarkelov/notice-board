4d82d9f50f70915de7883245816216e3fdbd5327
andrevmarkelov <andrev_markelov@mail.ru>
2024-06-01 16:24:14 +0300

    adding seeder

--- database/factories/AdFactory.php
+++ database/factories/AdFactory.php
@@ -19,15 +19,29 @@ class AdFactory extends Factory
      */
     public function definition(): array
     {
+        $images =[
+            'https://m.media-amazon.com/images/I/41WpqIvJWRL._AC_UY436_QL65_.jpg',
+            'https://m.media-amazon.com/images/I/61ghDjhS8vL._AC_UY436_QL65_.jpg',
+            'https://m.media-amazon.com/images/I/61c1QC4lF-L._AC_UY436_QL65_.jpg',
+            'https://m.media-amazon.com/images/I/710VzyXGVsL._AC_UY436_QL65_.jpg',
+            'https://m.media-amazon.com/images/I/61EPT-oMLrL._AC_UY436_QL65_.jpg',
+            'https://m.media-amazon.com/images/I/71r3ktfakgL._AC_UY436_QL65_.jpg',
+            'https://m.media-amazon.com/images/I/61CqYq+xwNL._AC_UL640_QL65_.jpg',
+            'https://m.media-amazon.com/images/I/71cVOgvystL._AC_UL640_QL65_.jpg',
+            'https://m.media-amazon.com/images/I/71E+oh38ZqL._AC_UL640_QL65_.jpg',
+            'https://m.media-amazon.com/images/I/61uSHBgUGhL._AC_UL640_QL65_.jpg',
+            'https://m.media-amazon.com/images/I/71nDK2Q8HAL._AC_UL640_QL65_.jpg'
+        ];
+
         return [
             'title' => $this->faker->sentence,
             'description' => $this->faker->paragraph,
             'price' => $this->faker->randomFloat(2, 10, 100),
-            'photos' => json_encode([
-                $this->faker->url,
-                $this->faker->url,
-                $this->faker->url,
-            ]),
+            'photos' => [
+                $images[rand(0, 10)],
+                $images[rand(0, 10)],
+                $images[rand(0, 10)],
+            ],
         ];
     }
 }
